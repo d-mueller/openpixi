cd39febab3e14b6f291958128b0a851d6f254120
David Müller <monolithu@gmail.com>
2016-02-22 11:08:38 +0100

    Implemented Wong1DPoissonSolver.
    This Poisson solver works in 1D and assumes initial gauge fields to be zero. This should be ok for particles which are at rest at the start of the simulation.

---
+++ pixi/src/main/java/org/openpixi/pixi/physics/fields/Wong1DPoissonSolver.java
@@ -0,0 +1,52 @@
+package org.openpixi.pixi.physics.fields;
+
+import org.openpixi.pixi.math.ElementFactory;
+import org.openpixi.pixi.physics.gauge.DoubleFFTWrapper;
+import org.openpixi.pixi.physics.grid.Grid;
+
+public class Wong1DPoissonSolver implements PoissonSolver {
+
+	int numberOfCells;
+	double as;
+
+	public void solve (Grid g) {
+		numberOfCells = g.getTotalNumberOfCells();
+		as = g.getLatticeSpacing();
+		ElementFactory factory = g.getElementFactory();
+		DoubleFFTWrapper fft = new DoubleFFTWrapper(g.getNumCells());
+		// Assume that gauge fields are zero initially. The Gauss constraint then reduces to the abelian case.
+		for (int j = 0; j < factory.numberOfComponents; j++) {
+			// Fourier transform charge density and solve in momentum space.
+			double[] fftArray = new double[fft.getFFTArraySize()];
+			for (int i = 0; i < numberOfCells; i++) {
+				fftArray[fft.getFFTArrayIndex(i)] = g.getRho(i).get(j);
+			}
+			fft.complexForward(fftArray);
+
+			for (int i = 1; i < numberOfCells; i++) {
+				double invLaplace = 1.0 / computeLatticeMomentumSquared(i);
+				fftArray[fft.getFFTArrayIndex(i)] *= invLaplace;
+				fftArray[fft.getFFTArrayIndex(i) + 1] *= invLaplace;
+			}
+			fftArray[0] = 0.0;
+			fftArray[1] = 0.0;
+
+			// Inverse Fourier transform.
+			fft.complexInverse(fftArray, true);
+
+			// Compute chromoelectric field from potential.
+			for (int i = 1; i < numberOfCells; i++) {
+				int l = g.shift(i, 0, -1);
+				int r = g.shift(i, 0, 1);
+				double E = (fftArray[fft.getFFTArrayIndex(l)] - fftArray[fft.getFFTArrayIndex(r)]) / (2 * as);
+				g.getE(i, 0).set(j, E);
+			}
+		}
+
+	}
+
+	private double computeLatticeMomentumSquared(int cellIndex) {
+		return 2.0 * (1.0 - Math.cos((2.0 * Math.PI * cellIndex) / numberOfCells)) / (as * as);
+	}
+
+}
