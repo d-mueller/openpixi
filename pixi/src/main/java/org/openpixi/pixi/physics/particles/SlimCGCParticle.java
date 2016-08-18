7613a118235c080e5b47ece9186d59fdf5c5e6a8
David Mueller <david.mueller@tuwien.ac.at>
2016-08-18 11:57:54 +0200

    Duplicated stuff for new SlimCGCParticle class.

---
+++ pixi/src/main/java/org/openpixi/pixi/physics/particles/SlimCGCParticle.java
@@ -0,0 +1,41 @@
+package org.openpixi.pixi.physics.particles;
+
+import org.openpixi.pixi.math.ElementFactory;
+import org.openpixi.pixi.math.GroupElement;
+
+/**
+ * This particle class is used for CGC simulations where the particles move in a certain direction along grid lines
+ * without being affected by a force. The color charge is parallel transported along the fixed trajectory.
+ */
+public class SlimCGCParticle extends YangMillsParticle {
+
+	public int direction;
+	public GroupElement U;
+	public boolean updateCharge;
+
+	public SlimCGCParticle(int numberOfDimensions, int numberOfColors, int direction) {
+		super(numberOfDimensions, numberOfColors);
+		this.direction = direction;
+		this.U = (new ElementFactory(numberOfColors)).groupIdentity();
+		this.updateCharge = true;
+	}
+
+	public IParticle copy() {
+		SlimCGCParticle p = new SlimCGCParticle(this.numberOfDimensions, this.numberOfColors, this.direction);
+		p.U = this.U.copy();
+
+		for (int i = 0; i < this.numberOfDimensions; i++) {
+			p.pos0[i] = this.pos0[i];
+			p.pos1[i] = this.pos1[i];
+			p.vel[i] = this.vel[i];
+			p.acc[i] = this.acc[i];
+		}
+
+		p.setRadius(this.r);
+		p.setDisplayColor(this.col);
+		p.updateCharge = this.updateCharge;
+
+		return p;
+	}
+
+}
